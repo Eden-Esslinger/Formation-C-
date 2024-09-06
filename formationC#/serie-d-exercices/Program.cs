6077daa78333d974d6c9438152e17df4f61dbf23
Formation <Formation@DESKTOP-TSCM4A7>
2024-09-06 11:07:19 +0200

    premier commit

---
+++ formationC#/serie-d-exercices/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace serie_d_exercices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
