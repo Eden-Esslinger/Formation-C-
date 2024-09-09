4d6c546919696f8964e0637b30a1f41c4719a745
Formation <Formation@DESKTOP-TSCM4A7>
2024-09-09 13:32:44 +0200

    exo 2

--- formationC#/serie-d-exercices/Serie I/Ex2_SpeakingClock.cs
+++ formationC#/serie-d-exercices/Serie I/Ex2_SpeakingClock.cs
@@ -11,17 +11,44 @@ namespace Serie_I
         public static string GoodDay(int heure)
         {
             switch (heure)
-            {
-                case heure when (heure > 0 && heure < 6):
-                 Console.WriteLine("Il est" + heure + "H, Merveilleuse nuit!");
+            { // de minuit à avant 6 heures du matin
+                case 0:
+                case 1:
+                case 2:
+                case 3:
+                case 4:
+                case 5:
+                    Console.WriteLine("Il est " + heure + " H, Merveilleuse nuit!");
                     break;
-                case ==6 && <12:
-                        Console.WriteLine("Il est" + heure + "H, Bonne matinée!");
+                // de 6 h du matin à avant midi
+                case 6:
+                case 7:
+                case 8:
+                case 9:
+                case 10:
+                case 11:
+                    Console.WriteLine("Il est " + heure + " H, Bonne matinée!");
                     break;
-                case == 12:
-                    Console.WriteLine("Il est" + heure + "H, Bon appétit!");
+                case 12: // a midi
+                    Console.WriteLine("Il est " + heure + " H, Bon appétit!");
                     break;
-                case 
+                // entre 13h et 18h
+                case 13:
+                case 14:
+                case 15:
+                case 16:
+                case 17:
+                case 18:
+                    Console.WriteLine("Il est " + heure + " H, Profitez de votre après-midi!");
+                    break;
+                // après 18h
+                case 19:
+                case 20:
+                case 21:
+                case 22:
+                case 23:
+                    Console.WriteLine("Il est " + heure + " H, Passez une bonne soirée!");
+                    break;
             }
             return string.Empty;
         }
