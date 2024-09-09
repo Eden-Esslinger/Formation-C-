2cc75783c1cfdd8afd2f53f6d80539453dfe24e6
Formation <Formation@DESKTOP-TSCM4A7>
2024-09-09 12:20:44 +0200

    premier exos

--- formationC#/serie-d-exercices/Serie I/Ex1_ElementaryOperations.cs
+++ formationC#/serie-d-exercices/Serie I/Ex1_ElementaryOperations.cs
@@ -1,37 +1,13 @@
-﻿using Serie_I;
-using System;
+﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace serie_d_exercices
-{
-    internal class Program
-    {
-        static void Main(string[] args)
-        {
-            ElementaryOperations.BasicOperation(2, 3, 'L');
-            ElementaryOperations.BasicOperation(2, 3, '/');
-            ElementaryOperations.BasicOperation(2, 0, '/');
-            ElementaryOperations.BasicOperation(2, 0, '*');
-            ElementaryOperations.BasicOperation(2, 5, '*');
-            ElementaryOperations.IntegerDivision(2, 5);
-            ElementaryOperations.IntegerDivision(20, 5);
-            ElementaryOperations.IntegerDivision(2, 0);
-            ElementaryOperations.Pow(2, 3);
-            ElementaryOperations.Pow(2, -3);
-            ElementaryOperations.Pow(2, 0);
-            Console.ReadKey();
-        }
-    }
-}
 namespace Serie_I
 {
     public static class ElementaryOperations
     {
-
         public static void BasicOperation(int a, int b, char operation)
         {
             int c;
@@ -66,7 +42,7 @@ namespace Serie_I
                 }
             }
         }
-        
+
         public static void IntegerDivision(int a, int b)
         {
             switch (b)
@@ -84,25 +60,23 @@ namespace Serie_I
                     { Console.WriteLine($"{a} = {q} * {b} + {r}"); }
                     break;
             }
-
         }
 
-            public static void Pow(int a, int b)
-            {
+        public static void Pow(int a, int b)
+        {
             int produit = a;
             if (b < 0)
             { Console.WriteLine($"{a} ^ {b} = Operation invalide."); }
             else if (b == 0)
             { Console.WriteLine($"{a} ^ {b} = 1"); }
             else
-            { for (int n = 1; n < b; n++)
+            {
+                for (int n = 1; n < b; n++)
                 {
                     produit *= a;
                 }
                 Console.WriteLine($"{a} ^ {b} = {produit}");
             }
-            } 
-
-        
+        }
     }
 }
---
+++ formationC#/serie-d-exercices/Serie I/Ex2_SpeakingClock.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_I
+{
+    public static class SpeakingClock
+    {
+        public static string GoodDay(int heure)
+        {
+            switch (heure)
+            {
+                case heure when (heure > 0 && heure < 6):
+                 Console.WriteLine("Il est" + heure + "H, Merveilleuse nuit!");
+                    break;
+                case ==6 && <12:
+                        Console.WriteLine("Il est" + heure + "H, Bonne matinée!");
+                    break;
+                case == 12:
+                    Console.WriteLine("Il est" + heure + "H, Bon appétit!");
+                    break;
+                case 
+            }
+            return string.Empty;
+        }
+    }
+}
