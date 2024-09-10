f87c077486eb6e2df4142fc8a706e45ae4619ade
Formation <Formation@DESKTOP-TSCM4A7>
2024-09-10 11:19:08 +0200

    exo 4

--- formationC#/serie-d-exercices/Serie I/Ex3_Pyramid.cs
+++ formationC#/serie-d-exercices/Serie I/Ex3_Pyramid.cs
@@ -9,31 +9,28 @@ namespace Serie_I
     public static class Pyramid
     {
         public static void PyramidConstruction(int n, bool isSmooth)
-        {
-            //  int blocs = 0;
-          //  for (int j = 1; j <= 2 * j - 1; j++)
-           // {
-                //    blocs = 2 * j - 1;
-           // }
-            for (int i = 1; i <= n; i++) 
+        {// nombre de blocs pour j = 2 * j - 1
+         // nombre total = n ^ 2
+         // sommet de la pyramide = n
+         // gauche = n - i + 1 et droite = n + i - 1
+            for (int i = 1; i <= n; i++) // niveau de la pyramide
             {
-                for (int j = 1; j <= 2 * n - 1; j++)
+                for (int j = 1; j <= 2 * n - 1; j++) // longueur pyramide
                 {
-                    if (j >= n - i + 1 && j <= n + i - 1)
-                    {
-                        if (isSmooth == true)
+                    if (j >= n - i + 1 && j <= n + i - 1) // borne où écrire + ou - 
+                        if (isSmooth == true) // booleen vrai
                         {
                             Console.Write("+");
                         }
-                        else if (i % 2 == 0)
+                        else if (i % 2 == 0) // niveau pair, et booleen faux
                         {
                             Console.Write("-");
                         }
                         else
                         { Console.Write("+"); }
-                    }
+                    
                     else
-                    { Console.Write(" "); }
+                    { Console.Write(" "); } 
                 }
                     Console.WriteLine();
                 
--- formationC#/serie-d-exercices/Serie I/Ex4_Factorial.cs
+++ formationC#/serie-d-exercices/Serie I/Ex4_Factorial.cs
@@ -10,14 +10,30 @@ namespace Serie_I
     {
         public static int Factorial_(int n)
         {
-            //TODO
-            return -1;
+            int facto = 1;
+            if (n < 0) // si negatif, retour -1
+                return -1;
+            else if (n == 0) // si = 0, factorielle = 1
+                return 1;
+            while (n >= 1) // tant que n n'est pas égale à 0, on calcule la factorielle de n
+            { facto *= n;
+                n--;
+            }
+            return facto;
         }
 
         public static int FactorialRecursive(int n)
         {
-            //TODO
-            return -1;
+            if (n < 0) // si negatif, retour -1
+                return -1;
+            else if (n == 0) // si = 0, factorielle = 1
+                return 1;
+            else  // calcul de la factorielle de n
+            {
+                return n * FactorialRecursive(n - 1);
+                
+            }
+           
         }
     }
 }
