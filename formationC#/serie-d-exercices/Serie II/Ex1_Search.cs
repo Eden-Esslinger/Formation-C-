6e4478fe131d1bb94f818c1f3f4eead58b19cf3a
Formation <Formation@DESKTOP-TSCM4A7>
2024-09-10 17:17:52 +0200

    serie 2 exo 1

---
+++ formationC#/serie-d-exercices/Serie II/Ex1_Search.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Serie_II
+{
+    public static class Search
+    {
+        public static int LinearSearch(int[] tableau, int valeur)
+        {
+            for (int i = 0; i < tableau.Length ; i++ )
+            {
+                if (valeur == tableau[i])
+                    return i;
+                else if (tableau[i] != valeur)
+
+                    return -1;
+                else
+                    return -1;
+            }
+                
+        }
+
+        public static int BinarySearch(int[] tableau, int valeur)
+        {
+            //TODO
+            return -1;
+        }
+    }
+}
