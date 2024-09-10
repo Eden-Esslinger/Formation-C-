af8b2385931f281c28aa84bbfcf2e38d405c58fb
Formation <Formation@DESKTOP-TSCM4A7>
2024-09-10 10:19:19 +0200

    exo 3

--- formationC#/serie-d-exercices/Serie I/Ex3_Pyramid.cs
+++ formationC#/serie-d-exercices/Serie I/Ex3_Pyramid.cs
@@ -17,11 +17,26 @@ namespace Serie_I
            // }
             for (int i = 1; i <= n; i++) 
             {
-                for (int j = 1; j <= n; j++)
-                 
-
-                { Console.Write("+"); }
-                Console.WriteLine();
+                for (int j = 1; j <= 2 * n - 1; j++)
+                {
+                    if (j >= n - i + 1 && j <= n + i - 1)
+                    {
+                        if (isSmooth == true)
+                        {
+                            Console.Write("+");
+                        }
+                        else if (i % 2 == 0)
+                        {
+                            Console.Write("-");
+                        }
+                        else
+                        { Console.Write("+"); }
+                    }
+                    else
+                    { Console.Write(" "); }
+                }
+                    Console.WriteLine();
+                
               //  j++;
                 
             }      
