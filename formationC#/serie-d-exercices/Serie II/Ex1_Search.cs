cc6f506ed9d9ede2a5890c0f081ba7babeb136f2
Formation <Formation@DESKTOP-TSCM4A7>
2024-09-11 11:44:44 +0200

    exo 1 serie 2

--- formationC#/serie-d-exercices/Serie II/Ex1_Search.cs
+++ formationC#/serie-d-exercices/Serie II/Ex1_Search.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Diagnostics.Eventing.Reader;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -10,23 +11,73 @@ namespace Serie_II
     {
         public static int LinearSearch(int[] tableau, int valeur)
         {
-            for (int i = 0; i < tableau.Length ; i++ )
+            for (int i = 0; i < tableau.Length; i++)
             {
-                if (valeur == tableau[i])
+                if (tableau.Length == 0)
+                    return -1;
+
+                else if (valeur == tableau[i])
+                {
                     return i;
-                else if (tableau[i] != valeur)
+                    break;
+                }
+                else if (i == tableau.Length)
 
                     return -1;
+
+
                 else
-                    return -1;
+                    continue;
             }
-                
+            return -1; 
         }
 
         public static int BinarySearch(int[] tableau, int valeur)
         {
-            //TODO
-            return -1;
+            int i = tableau.Length / 2;
+            if (tableau.Length == 0)
+                return -1;
+
+            if (valeur == tableau[i])
+            {
+                return i;
+               
+            }
+            else if (valeur < tableau[i])
+            {
+                for (int j = 0; j < tableau.Length / 2; j++)
+                {
+                    if (valeur == tableau[j])
+                    {
+                        return j;
+                        break;
+                    }
+                    else if (j == tableau.Length / 2)
+
+                        return -1;
+
+                    else
+                        continue;
+                }
+            }
+            else if (valeur > tableau[i])
+            {
+                for (int k = tableau.Length / 2; k < tableau.Length; k++)
+                {
+                    if (valeur == tableau[k])
+                    {
+                        return k;
+                        break;
+                    }
+                    else if (k == tableau.Length)
+
+                        return -1;
+
+                    else
+                        continue;
+                }
+            }
+                return -1;
         }
     }
 }
